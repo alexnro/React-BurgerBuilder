f4909596efc0d1997aa252fa88a416ed230318b1
alexnro <alejandro.navarro@ETOOL.TECH>
2019-05-03 12:36:03 +0200

    Added generic method to output ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -15,7 +15,7 @@ const burger = (props) => {
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
+            {transformedIngredients }
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
