ca8a37aebf48a0fae898cacf745671be8322579d
alexnro <alejandro.navarro@ETOOL.TECH>
2019-05-07 13:33:51 +0200

    add style to sideDraw menu button

--- src/components/Navigation/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/DrawerToggle/DrawerToggle.js
@@ -3,8 +3,10 @@ import React from 'react';
 import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => (
-    <div className={classes.Menu} onClick={props.clicked}>
-        <p>MENU</p>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 );
 
