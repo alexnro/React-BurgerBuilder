9d20c41764dc68f7427a91476e03576d2d060864
alexnro <alejandro.navarro@ETOOL.TECH>
2019-05-17 13:25:01 +0200

    Installing redux and react-redux

---
+++ src/store/reducer.js
@@ -0,0 +1,11 @@
+import * as actionType from './actions';
+
+const initialState = {
+    ingredients: null,
+        totalPrice: 4
+};
+const reducer = (state = initialState, action) => {
+    
+};
+
+export default reducer;
